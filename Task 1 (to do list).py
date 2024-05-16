4ca5dbcaca65c053756599a870796a8f287f814f
gowthamikunte14 <162871735+gowthamikunte14@users.noreply.github.com>
2024-05-16 13:13:55 +0530

    Add files via upload

---
+++ Task 1 (to do list).py
@@ -0,0 +1,61 @@
+tasks = []
+
+
+def addTask():
+    task = input("please enter a task: ")
+    task.append(task)
+    print(f"Task '{task}' added to the list.")
+
+
+def listTasks():
+    if not tasks:
+        print("There are no tasks currently.")
+    else:
+        print("CurrentTasks:")
+        for index, task in enumerate(tasks):
+            print(f"Task #{index}. {tasks}")
+    
+
+def deleteTask():
+        listTasks()
+        try:
+            taskToDelete = int(input("Enter the # to delete: "))
+            if taskToDelete >=0 and taskToDelete < len(tasks):
+                tasks.pop(taskToDelete)
+                print(f"Task {taskToFDelete} has been removed.")
+            else:
+                print(f"Task #{taskToDelete} was not found.")    
+        except:
+            print("invalid input.")
+
+       
+
+
+if __name__=="__main__":
+    ### create a loop to run the app
+    print("welcome to the to do list app:)")
+    while True:
+        print("\n")
+        print("please select one of the following options")
+        print("------------------------------------------")
+        print("1. Add a new task")
+        print("2. Delete a task")
+        print("3. List tasks ")
+        print("4. Quit")
+
+        choice = input("enter your choice: ")
+
+        if(choice == "1"):
+            addTask()
+        elif(choice == "2"):
+            deleteTask()
+        elif(choice == "3"):
+            listTasks()
+        elif(choice == "4"):
+            break
+        else:
+            print("Invalid input. Please try again.")
+
+            print("Goodbye")
+
+
